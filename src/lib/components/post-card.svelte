1a88cde3053bcb402d6a3cc6bf71cceafece49ef
spences10 <spences10apps@gmail.com>
2021-07-18 08:59:09 +0100

    refactor: :recycle: move posts card to own component

---
+++ src/lib/components/post-card.svelte
@@ -0,0 +1,21 @@
+<script>
+  import ButtonLink from '$lib/components/ButtonLink.svelte'
+  import { format } from 'date-fns'
+
+  export let post
+</script>
+
+<article class="py-8 first:pt-0">
+  <div>
+    <h1 class="!mt-0 !mb-1">
+      <a href={`/posts/${post.slug}`}>{post.title}</a>
+    </h1>
+    <time>{format(new Date(post.date), 'MMMM d, yyyy')}</time>
+    •
+    <span>{post.readingTime.text}</span>
+  </div>
+  <div>{@html post.previewHtml}</div>
+  <div class="flex justify-end w-full">
+    <ButtonLink href={`/posts/${post.slug}`}>Read More</ButtonLink>
+  </div>
+</article>
