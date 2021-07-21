32e3bf1eb12bb086ec56300379a525bd29589cbe
spences10 <spences10apps@gmail.com>
2021-07-21 09:08:38 +0100

    feat: :sparkles: update with links

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,13 +1,13 @@
 <script context="module">
-  export function load({ error, status }) {
+  export function load({ error, status, page: { path } }) {
     return {
-      props: { error, status },
+      props: { error, status, path },
     }
   }
 </script>
 
 <script>
-  export let error, status
+  export let error, status, path
 </script>
 
 <svelte:head>
@@ -15,5 +15,10 @@
 </svelte:head>
 
 <h1>{status}</h1>
-<p>That's a nop! 😭</p>
 <p>{error.message}</p>
+<p>That's a nop! 😭</p>
+<p>It looks like <code>{path}</code> doesn't exist</p>
+<p>
+  Maybe check out the <a href="/posts/">posts</a> page? Or go to the
+  <a href="/">home</a> page.
+</p>
