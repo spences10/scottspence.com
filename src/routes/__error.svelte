813c485ecebc8f52daccd6c912b9d66566e4315c
spences10 <spences10apps@gmail.com>
2021-07-21 08:23:27 +0100

    feat: :sparkles: move posts index from index page
    move to posts add redirect for garden > posts

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: { error, status },
+    }
+  }
+</script>
+
+<script>
+  export let error, status
+</script>
+
+<svelte:head>
+  <title>{status}</title>
+</svelte:head>
+
+<h1>{status}</h1>
+<p>That's a nop! 😭</p>
+<p>{error.message}</p>
