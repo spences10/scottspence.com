9434fc3b591ff8a7535cedbd050ddc3ef3d7d2ec
spences10 <spences10apps@gmail.com>
2021-07-22 20:46:01 +0100

    feat: :sparkles: import about into index for now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,23 @@
-hello
+<script context="module">
+  export async function load({ page }) {
+    try {
+      const Post = await import(`../../copy/about.md`)
+      return {
+        props: {
+          Post: Post.default,
+        },
+      }
+    } catch (e) {
+      return {
+        status: 404,
+        error: 'Uh oh!',
+      }
+    }
+  }
+</script>
+
+<script>
+  export let Post
+</script>
+
+<Post />
