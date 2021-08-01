49cb95fd954a65223a453605dd7de8367ab0cdd7
spences10 <spences10apps@gmail.com>
2021-08-01 10:54:37 +0100

    chore: :memo: minor formatting

--- src/lib/components/table-of-contents.svelte
+++ src/lib/components/table-of-contents.svelte
@@ -6,7 +6,10 @@
   <aside
     class="rounded-md shadow w-64 table-of-contents lg:block hidden"
   >
-    <h3 class="!font-extrabold !text-lg !mt-2 !mb-0" id="table-of-contents">
+    <h3
+      class="!font-extrabold !text-lg !mt-2 !mb-0"
+      id="table-of-contents"
+    >
       Table of Contents
     </h3>
     <ul class="max-h-72 overflow-auto !mb-0">
