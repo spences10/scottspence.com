e5d728a0d9f8891d512a3288815388e166395571
spences10 <spences10apps@gmail.com>
2021-08-06 10:52:35 +0100

    fix: :bug: set width on hero image

--- src/lib/components/landing-hero.svelte
+++ src/lib/components/landing-hero.svelte
@@ -1,10 +1,10 @@
-<div class="mb-44 relative 2xl:-mx-96 lg:-mx-52 xl:-mx-64">
+<div class="mb-44 relative lg:-mx-52 xl:-mx-64 2xl:-mx-96">
   <div class="hero">
     <div class="flex-col hero-content lg:flex-row-reverse">
       <img
         src="/mug-face.png"
         alt="Cartoon face Scott"
-        class="rounded-full max-w-sm shadow-2xl"
+        class="rounded-full h-full max-w-sm w-full shadow-2xl"
       />
       <div class="">
         <h1 class="font-bold mb-5 text-5xl">
