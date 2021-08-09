2a7f4d6ed9e49943a1ee4edc4382261b14181e16
spences10 <spences10apps@gmail.com>
2021-08-09 12:07:54 +0100

    feat: :sparkles: optimise local image

--- src/lib/components/landing-hero.svelte
+++ src/lib/components/landing-hero.svelte
@@ -5,7 +5,7 @@
 <div class="mb-44 relative lg:-mx-52 xl:-mx-64 2xl:-mx-96">
   <div class="hero">
     <div class="flex-col hero-content lg:flex-row-reverse p-0">
-      <Lazy height={300}>
+      <Lazy height={330}>
         <img
           src="/mug-face.png"
           alt="Cartoon face Scott"
