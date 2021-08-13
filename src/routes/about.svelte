2ae30b78e2a40768c29a8f60e7f6221ca8e363ff
spences10 <spences10apps@gmail.com>
2021-08-13 21:29:15 +0100

    feat: :sparkles: add footer back

---
+++ src/routes/about.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+  export async function load({ page }) {
+    try {
+      const Copy = await import(`../../copy/about.md`)
+      return {
+        props: {
+          Copy: Copy.default,
+        },
+      }
+    } catch (e) {
+      return {
+        status: 404,
+        error: 'Uh oh!',
+      }
+    }
+  }
+</script>
+
+<script>
+  import Head from '$lib/components/head.svelte'
+  import { name } from '$lib/info'
+  import { ogImageUrl } from '$lib/og-image-url-build'
+
+  export let Copy
+</script>
+
+<Head
+  title={`About · ${name}`}
+  description={`Some info about ${name}`}
+  image={ogImageUrl(name, `scottspence.com`, `About`)}
+/>
+
+<svelte:component this={Copy} />
