5aff355ebf4d07d9e8abb9516995f9f866813591
spences10 <spences10apps@gmail.com>
2021-08-13 22:59:30 +0100

    style: :lipstick: style hero better

--- src/lib/components/landing-hero.svelte
+++ src/lib/components/landing-hero.svelte
@@ -1,28 +1,36 @@
-<script>
-  import Lazy from 'svelte-lazy'
-</script>
-
-<div class="mb-44 relative lg:-mx-52 xl:-mx-64 2xl:-mx-96">
+<div
+  class="mb-4 relative lg:-mx-40 lg:mb-44 lg:px-8 xl:-mx-64 xl:mb-[17rem] 2xl:-mx-60 2xl:mb-72"
+>
   <div class="hero">
-    <div class="flex-col hero-content lg:flex-row-reverse p-0">
-      <Lazy height={330}>
-        <img
-          src="/mug-face.png"
-          alt="Cartoon face Scott"
-          class="rounded-full h-full max-w-sm w-full shadow-2xl"
-        />
-      </Lazy>
-      <div class="">
+    <div class="flex-col p-0 hero-content lg:flex-row-reverse">
+      <img
+        src="/mug-face.png"
+        alt="Cartoon face Scott"
+        class="rounded-full max-w-sm w-full shadow-xl"
+      />
+      <div class="mr-28">
         <h1 class="font-bold mb-5 text-5xl">
           <span class="block">Scott Spence</span>
-          <span class="text-primary block"> Hello World! </span>
+          <span class="text-primary block">Hello World!</span>
         </h1>
         <p class="mb-5">
           This is my blog where I write about many things, including,
           but not limited to JavaScript, React, Gatsby, Toast,
           Tailwind, Svelte and many more web dev related topics.
         </p>
-        <!-- <button class="btn btn-primary">Get Started</button> -->
+        <p class="mb-5">
+          Check out that massive picture of my <a
+            href="https://www.cockneyrhymingslang.co.uk/slang/boat_race"
+            >boat race</a
+          >
+          ! What do you think? If you want to crack on then check out some
+          of the stuff I'm writing about on the
+          <a href="/posts">posts page</a>.
+        </p>
+        <p class="mb-5">
+          You can carry on scrolling for a bit more info about me if
+          you like.
+        </p>
       </div>
     </div>
   </div>
