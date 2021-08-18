470b9cc1bd3be9731e726a8076125206c186b785
spences10 <spences10apps@gmail.com>
2021-08-18 21:51:38 +0100

    feat: :construction: add analytics endpoint

---
+++ src/routes/analytics/index.json.js
@@ -0,0 +1,34 @@
+export async function get() {
+  try {
+    const getHeaders = new Headers()
+    getHeaders.append(
+      `Authorization`,
+      `Bearer ${import.meta.env.VITE_FATHOM_API_KEY}`
+    )
+    const encURI = encodeURI(
+      `[{"property": "pathname","operator": "is","value": "/"}]`
+    )
+    const res = await fetch(
+      `https://api.usefathom.com/v1/aggregations?entity=pageview&entity_id=${
+        import.meta.env.VITE_FATHOM_ID
+      }&aggregates=pageviews&field_grouping=pathname&filters=${encURI}`,
+      {
+        headers: getHeaders,
+      }
+    )
+
+    return {
+      status: 200,
+      body: {
+        analytics: await res.json(),
+      },
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: {
+        error: 'Big oof! Sorry',
+      },
+    }
+  }
+}
