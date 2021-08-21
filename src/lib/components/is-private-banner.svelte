fc0afd420d44a45cc25b044b6f75020dd7be8c0e
spences10 <spences10apps@gmail.com>
2021-08-21 11:02:13 +0100

    style: :lipstick: all-prose on p

--- src/lib/components/is-private-banner.svelte
+++ src/lib/components/is-private-banner.svelte
@@ -1,12 +1,12 @@
-<div class="mb-8 py-8">
+<div class="py-8">
   <div class="mx-auto max-w-full">
-    <div class="p-2 bg-red-600 rounded-lg shadow-lg sm:p-3">
+    <div class="rounded-lg bg-red-600 shadow-lg p-2 sm:p-3">
       <div class="flex flex-wrap items-center justify-between">
-        <div class="flex flex-1 items-center w-0">
-          <span class="bg-primary-400 flex p-2 rounded-lg">
-            <!-- <EyeOffIcon class="w-4 h-4" /> -->
+        <div class="flex flex-1 w-0 items-center">
+          <span class="rounded-lg flex bg-primary-400 p-2">
+            <!-- <EyeOffIcon class="h-4 w-4" /> -->
           </span>
-          <p class="ml-3 w-full text-center font-medium">
+          <p class="font-medium text-center ml-3 w-full all-prose">
             This is a private post 🤫
           </p>
         </div>
