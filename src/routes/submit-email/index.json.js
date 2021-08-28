b82af916f14298144e14ba2b357962e867d74fbd
spences10 <spences10apps@gmail.com>
2021-08-28 15:39:38 +0100

    chore: :wrench: nojs flag

--- src/routes/submit-email/index.json.js
+++ src/routes/submit-email/index.json.js
@@ -1,6 +1,6 @@
 export async function post(req) {
   const { email } = JSON.parse(req.body)
-  await fetch('https://spences10.substack.com/api/v1/free', {
+  await fetch('https://spences10.substack.com/api/v1/free?nojs=true', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
