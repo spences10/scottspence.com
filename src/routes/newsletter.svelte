f89d94309f82340a5ea6a7de4639761332067569
spences10 <spences10apps@gmail.com>
2021-08-28 16:57:08 +0100

    revert: :rewind: revert signup form to janky substack one 🥲

--- src/routes/newsletter.svelte
+++ src/routes/newsletter.svelte
@@ -18,7 +18,6 @@
 
 <script>
   import Head from '$lib/components/head.svelte'
-  import NewsletterSignup from '$lib/components/newsletter-signup.svelte'
   import { name, website } from '$lib/info'
   import { ogImageUrl } from '$lib/og-image-url-build'
 
@@ -36,4 +35,12 @@
   <svelte:component this={Copy} />
 </div>
 
-<NewsletterSignup />
+<iframe
+  title="substack_subscribe"
+  src="https://spences10.substack.com/embed"
+  width="100%"
+  height="320"
+  style="border:1px solid #EEE; background:transparent;"
+  frameborder="0"
+  scrolling="no"
+/>
