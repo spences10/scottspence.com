38bc0aaaf3b1b87af11c95786e734687de415944
spences10 <spences10apps@gmail.com>
2021-09-06 09:20:01 +0100

    refactor: :recycle: add better error handling on the endpoint

--- src/routes/submit-email/index.json.js
+++ src/routes/submit-email/index.json.js
@@ -2,23 +2,46 @@ export async function post(req) {
   const { email } = JSON.parse(req.body)
   const REVUE_API_KEY = process.env['REVUE_API_KEY']
 
-  await fetch('https://www.getrevue.co/api/v2/subscribers', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${REVUE_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email,
-      first_name: '',
-      last_name: '',
-      double_opt_in: false,
-    }),
-  })
-  return {
-    status: 200,
-    body: JSON.stringify({
-      message: 'Email sent!',
-    }),
+  try {
+    const res = await fetch(
+      'https://www.getrevue.co/api/v2/subscribers',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Token ${REVUE_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          first_name: '',
+          last_name: '',
+          double_opt_in: false,
+        }),
+      }
+    )
+
+    if (res.ok) {
+      return {
+        status: 200,
+        body: JSON.stringify({
+          message: 'email sent!',
+        }),
+      }
+    }
+    if (res.status === 400) {
+      return {
+        status: 400,
+        body: JSON.stringify({
+          message: 'bad request',
+        }),
+      }
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: JSON.stringify({
+        message: 'something went wrong with the email submit!',
+      }),
+    }
   }
 }
