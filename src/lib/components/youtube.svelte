6aea25a36f5d415e31ff29d5905d8bbdb26c5ef0
spences10 <spences10apps@gmail.com>
2021-09-11 18:28:06 +0100

    refactor: :art: move get padding out to own file
    add Vimeo component

--- src/lib/components/youtube.svelte
+++ src/lib/components/youtube.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { getPadding } from '$lib/utils'
   import GeneralObserver from './general-observer.svelte'
 
   export let width = 560
@@ -15,17 +16,6 @@
   const tM = m * 60
 
   const startTime = tH + tM + s
-
-  export const getPadding = aspectRatio => {
-    const config = {
-      '1:1': `padding-top: 100%;`,
-      '16:9': `padding-top: 56.25%;`,
-      '4:3': `padding-top: 75%;`,
-      '3:2': `padding-top: 66.66%;`,
-      '8.5': `padding-top: 62.5%;`,
-    }
-    return config[aspectRatio]
-  }
 
   const baseUrl = `https://www.youtube-nocookie.com/embed/`
   const src = `${baseUrl}${
