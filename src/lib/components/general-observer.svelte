71ab7810992f0d975e5f9969d52b406f10168c40
spences10 <spences10apps@gmail.com>
2021-09-11 18:09:30 +0100

    feat: :sparkles: add general observer

---
+++ src/lib/components/general-observer.svelte
@@ -0,0 +1,23 @@
+<script>
+  import viewport from '$lib/use-viewport-action'
+
+  export let height = 100
+  export let width = 100
+
+  let intersecting
+</script>
+
+<div
+  use:viewport
+  on:enterViewport={() => (intersecting = true)}
+  on:exitViewport={() => (intersecting = false)}
+>
+  {#if intersecting}
+    <div class="flex justify-center mb-12" style={`width: ${width}`}><slot /></div>
+  {:else}
+    <div
+      class="flex justify-center mb-12"
+      style={`height:${height}px;width: 100%`}
+    />
+  {/if}
+</div>
