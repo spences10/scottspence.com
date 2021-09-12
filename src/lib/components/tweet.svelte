70b61e58b992923d9821e54045d2153699caddb4
spences10 <spences10apps@gmail.com>
2021-09-12 19:50:27 +0100

    feat: :art: tweet component work

--- src/lib/components/tweet.svelte
+++ src/lib/components/tweet.svelte
@@ -1,5 +1,20 @@
 <script>
+  import GeneralObserver from './general-observer.svelte'
+
   export let tweetLink = ''
 </script>
 
-<div>{tweetLink}</div>
+<svelte:head>
+  <script
+    async
+    src="https://platform.twitter.com/widgets.js"
+    charset="utf-8"></script>
+</svelte:head>
+
+<GeneralObserver>
+  <blockquote class="twitter-tweet">
+    <a class="twitter-tweet" href={`https://twitter.com/${tweetLink}`}
+      >Loading Tweet...</a
+    >
+  </blockquote>
+</GeneralObserver>
