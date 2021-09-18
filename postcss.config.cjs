baff0979c5c897d34d2101773fdba839dd528ba6
spences10 <spences10apps@gmail.com>
2021-09-18 19:51:56 +0100

    refactor: :recycle: remove focus-visible

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
-const focusVisible = require('postcss-focus-visible')
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
@@ -20,7 +19,5 @@ module.exports = {
       cssnano({
         preset: 'default',
       }),
-
-    focusVisible,
   ],
 }
