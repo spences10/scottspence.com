f64ae9e7fdccde66a20e77c4f2bef337ab1d3a34
spences10 <spences10apps@gmail.com>
2022-02-01 16:29:43 +0000

    style: :lipstick: simplify style on private banner

--- src/lib/components/is-private-banner.svelte
+++ src/lib/components/is-private-banner.svelte
@@ -1,16 +1,7 @@
-<div class="py-8">
-  <div class="mx-auto max-w-full">
-    <div class="rounded-lg bg-red-600 shadow-lg p-2 sm:p-3">
-      <div class="flex flex-wrap items-center justify-between">
-        <div class="flex flex-1 w-0 items-center">
-          <span class="rounded-lg flex bg-primary-400 p-2">
-            <!-- <EyeOffIcon class="h-4 w-4" /> -->
-          </span>
-          <p class="font-medium text-center ml-3 w-full all-prose">
-            This is a private post 🤫
-          </p>
-        </div>
-      </div>
-    </div>
+<div class="alert mb-8">
+  <div class="flex-1">
+    <p class="font-medium text-3xl text-center w-full">
+      This is a private post 🤫
+    </p>
   </div>
 </div>
