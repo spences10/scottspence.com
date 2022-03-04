76944c9ba6fd3e16184397f98db1ffce9f4132bf
spences10 <spences10apps@gmail.com>
2022-03-04 20:57:32 +0000

    feat: :sparkles: add SC component to portfolio page

--- src/lib/components/github-contributions.svelte
+++ src/lib/components/github-contributions.svelte
@@ -1,20 +1,31 @@
 <script>
-  let thisYear = new Date().getFullYear()
+  import * as SC from 'svelte-cubed'
+  import * as THREE from 'three'
+
+  export let contributions
+
+  let rows = 7
 </script>
 
-<div
-  class="flex justify-center md:-mx-24 lg:-mx-52 xl:-mx-64 2xl:-mx-96 md:h-[260px]"
->
-  <a
-    href={`https://ghui.vercel.app/heat.png?username=spences10&year=${thisYear}&interactive=true`}
-    target="_blank"
-    rel="noopener noreferrer"
-    class="hover:opacity-100"
-  >
-    <img
-      src={`https://ghui.vercel.app/heat.png?username=spences10&year=${thisYear}`}
-      alt="GitHub contributions heatmap"
-      loading="eager"
-    />
-  </a>
-</div>
+<article class="relative h-[48rem] w-[48rem]">
+  <SC.Canvas antialias alpha>
+    {#each contributions as contribution, index}
+      <SC.Mesh
+        scale={[1, contribution.value, 1]}
+        geometry={new THREE.BoxGeometry(1, 1, 1)}
+        material={new THREE.MeshStandardMaterial({
+          color: `hsl(133, 63%, ${contribution.value * 2}%)`,
+        })}
+        position={[
+          Math.floor(index / rows),
+          contribution.value / 2,
+          index % rows,
+        ]}
+      />
+    {/each}
+    <SC.PerspectiveCamera position={[15, 15, 100]} />
+    <SC.OrbitControls />
+    <SC.AmbientLight intensity={0.5} />
+    <SC.DirectionalLight intensity={0.5} position={[3, 5, -4]} />
+  </SC.Canvas>
+</article>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,10 +1,14 @@
 <script context="module">
-  export const load = async () => {
+  export const load = async ({ fetch }) => {
     try {
+      const res = await fetch('/github-contributions.json')
+      let contributions = await res.json()
+
       const Copy = await import(`../../copy/portfolio.md`)
       return {
         props: {
           Copy: Copy.default,
+          contributions,
         },
       }
     } catch (e) {
@@ -18,10 +22,12 @@
 
 <script>
   import Head from '@components/head.svelte'
+  import GithubContributions from '@lib/components/github-contributions.svelte'
   import { name, website } from '@lib/info'
   import { ogImageUrl } from '@lib/og-image-url-build'
 
   export let Copy
+  export let contributions
 </script>
 
 <Head
@@ -34,6 +40,8 @@
   )}
   url={`${website}/portfolio`}
 />
+
+<GithubContributions {contributions} />
 
 <div class="all-prose">
   <svelte:component this={Copy} />
