9ac443310dc9c898c733a0397b57504d5a906da3
spences10 <spences10apps@gmail.com>
2022-03-07 20:05:19 +0000

    refactor: :recycle: use daisyUI footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,45 +1,28 @@
 <script>
-  import { POPULAR_POSTS, SITE_LINKS, SOCIAL_LINKS } from '$lib/info'
+  import { POPULAR_POSTS,SITE_LINKS,SOCIAL_LINKS } from '$lib/info'
   import Eye from '@lib/icons/eye.svelte'
   import { trackGoal } from 'fathom-client'
 </script>
 
-<footer class="footer p-10 bg-primary inset-0 relative z-10">
-  <div class="">
-    <div class="rounded-full overflow-hidden h-14 mb-8 w-14">
-      <img
-        src="/mug-face.png"
-        alt="scott profile"
-        class="object-cover"
-      />
-    </div>
-    <p class="text-primary-content">
-      Built with SvelteKit, MDSveX and Tailwind
-    </p>
-    <p class="text-primary-content text-lg">
-      Coding code since Cody McCoderson said code! Check the about
-      section for more.
-    </p>
-  </div>
+<footer class="footer p-10 bg-primary text-primary-content">
   <div>
-    <span class="footer-title text-secondary-content"
-      >Popular Posts</span
-    >
+    <span class="footer-title">Popular Posts</span>
     {#each POPULAR_POSTS as post}
       <a
         sveltekit:prefetch
-        href={`/${post.slug}`}
         class="text-primary-content hover:opacity-50"
+        href={post.slug}
       >
         {post.title}
-        <span class="text-primary-content font-bold"
-          ><Eye /> {post.views}</span
-        >
+        <span class="text-primary-content font-bold">
+          <Eye />
+          {post.views}
+        </span>
       </a>
     {/each}
   </div>
   <div>
-    <span class="footer-title text-primary-content">Site Links</span>
+    <span class="footer-title">Site Links</span>
     {#each SITE_LINKS as link}
       <a
         sveltekit:prefetch
@@ -52,7 +35,7 @@
     {/each}
   </div>
   <div>
-    <span class="footer-title text-primary-content">Socials</span>
+    <span class="footer-title">Socials</span>
     {#each SOCIAL_LINKS as social}
       <a
         class="text-primary-content hover:opacity-50"
@@ -65,3 +48,4 @@
     {/each}
   </div>
 </footer>
+
