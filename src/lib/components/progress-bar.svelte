9de83cbde839b11120b2a6eda96b61b7e065dbe5
spences10 <spences10apps@gmail.com>
2022-12-25 09:31:24 +0000

    refactor: :recycle: add progress %

--- src/lib/components/progress-bar.svelte
+++ src/lib/components/progress-bar.svelte
@@ -3,14 +3,18 @@
   export let max = 100
   export let width = ``
   export let label = ``
+  let progress = (value / max * 100).toFixed()
 </script>
 
-<label for="progress" class="label -mb-5">
+<label for="progress" class="label -mb-5 -ml-1">
   <span class="label-text">{label}</span>
 </label>
 <progress
-  name="progress"
-  class={`progress progress-primary ${width}`}
-  {value}
-  {max}
+name="progress"
+class={`progress progress-primary ${width}`}
+{value}
+{max}
 />
+<label for="progress" class="label -mt-4 -ml-1">
+  <span class="label-text">{progress}%</span>
+</label>
