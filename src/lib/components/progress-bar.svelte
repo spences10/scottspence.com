3d566fa2a0a614fcab7e6296d80cfbf2d22a7c03
spences10 <spences10apps@gmail.com>
2022-12-25 08:29:49 +0000

    refactor: :recycle: add additional props

--- src/lib/components/progress-bar.svelte
+++ src/lib/components/progress-bar.svelte
@@ -1,6 +1,16 @@
 <script>
   export let value = 0
   export let max = 100
+  export let width = ``
+  export let label = ``
 </script>
 
-<progress class="progress progress-primary w-56" {value} {max}></progress>
+<label for="progress" class="label -mb-5">
+  <span class="label-text">{label}</span>
+</label>
+<progress
+  name="progress"
+  class={`progress progress-primary ${width}`}
+  {value}
+  {max}
+/>
