2c2f5bf1c574a0da33b4080fbd15f2a13ba56db2
spences10 <spences10apps@gmail.com>
2022-12-26 09:19:57 +0000

    refactor: :recycle: move width out to outer section element

--- src/lib/components/progress-bar.svelte
+++ src/lib/components/progress-bar.svelte
@@ -1,20 +1,22 @@
 <script>
   export let value = 0
   export let max = 100
-  export let width = ``
+  export let width = `w-full`
   export let label = ``
-  let progress = (value / max * 100).toFixed()
+  let progress = ((value / max) * 100).toFixed()
 </script>
 
-<label for="progress" class="label -mb-5 -ml-1">
-  <span class="label-text">{label}</span>
-</label>
-<progress
-name="progress"
-class={`progress progress-primary ${width}`}
-{value}
-{max}
-/>
-<label for="progress" class="label -mt-4 -ml-1">
-  <span class="label-text">{progress}%</span>
-</label>
+<section class={`${width}`}>
+  <label for="progress" class="label -mb-5 -ml-1">
+    <span class="label-text">{label}</span>
+  </label>
+  <progress
+    id="progress"
+    class="progress progress-primary w-full"
+    {value}
+    {max}
+  />
+  <label for="progress" class="label -mt-4 -ml-1">
+    <span class="label-text">{progress}%</span>
+  </label>
+</section>
