4b6fe7ea4fd9dd29a02a46a5ca0b96d5776fdc38
spences10 <spences10apps@gmail.com>
2022-12-31 14:16:06 +0000

    feat: :sparkles: add warning for posts over a year old

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -5,9 +5,14 @@
   import PopularPosts from '@components/popular-posts.svelte'
   import ShareWithTweet from '@components/share-with-tweet.svelte'
   import TableOfContents from '@components/table-of-contents.svelte'
+  import DateDistance from '@lib/components/date-distance.svelte'
   import { name, website } from '@lib/info'
   import { ogImageUrl } from '@lib/og-image-url-build'
-  import { differenceInDays, format } from 'date-fns'
+  import {
+    differenceInDays,
+    differenceInYears,
+    format,
+  } from 'date-fns'
   import { onMount } from 'svelte'
 
   // metadata
@@ -84,6 +89,35 @@
   </div>
   {#if isPrivate}
     <IsPrivateBanner />
+  {/if}
+
+  {#if differenceInYears(new Date(), new Date(date)) >= 1}
+    <div class="alert alert-warning shadow-lg">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="stroke-current flex-shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+          />
+        </svg>
+        <span>
+          Hey! Thanks for stopping by! Just a word of warning, this
+          post is
+          <span class="font-bold italic">
+            <DateDistance date={new Date(date)} /> old.
+          </span>
+          If there's technical information in here it's more than likely
+          out of date.
+        </span>
+      </div>
+    </div>
   {/if}
 
   <div class="all-prose mb-10">
