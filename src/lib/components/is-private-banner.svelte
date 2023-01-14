1e8433a05b7ec95bc633175f52298407d4c7d628
spences10 <spences10apps@gmail.com>
2023-01-14 16:23:14 +0000

    refactor: :recycle: update private post copy

--- src/lib/components/is-private-banner.svelte
+++ src/lib/components/is-private-banner.svelte
@@ -1,7 +1,11 @@
 <div class="alert mb-8">
-  <div class="flex-1">
+  <div class="flex flex-col w-full">
     <p class="font-medium text-3xl text-center w-full">
       This is a private post 🤫
+    </p>
+    <p class='text-center'>
+      This means it's probably incomplete and not intended for public
+      consumption.
     </p>
   </div>
 </div>
