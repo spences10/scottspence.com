4c4cc84fdfc8dff27f397272e92e38c209cf08d4
spences10 <spences10apps@gmail.com>
2023-01-23 20:58:46 +0000

    refactor: :recycle: rename to details-transition

---
+++ posts/2023/01/17/using-the-svelte-use-action-for-animations/details-transition.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { slide } from 'svelte/transition'
+  export let buttonText = ''
+  export let open = false
+</script>
+
+<section class="border not-prose">
+  <button
+    on:click={() => {
+      open = !open
+    }}
+  >
+    <div class="flex items-center text-left">
+      <span style="margin:0 1rem;" class="transition" class:open>
+        ▶
+      </span>
+      <p style="margin:1rem 0;">{buttonText}</p>
+    </div>
+  </button>
+  {#if open}
+    <div transition:slide>
+      <slot />
+    </div>
+  {/if}
+</section>
+
+<style>
+  .open {
+    transform: rotate(90deg);
+    transform-origin: center;
+  }
+</style>
