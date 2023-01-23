64e09393d38cc0620f53e7f60f6960a11ee1c222
spences10 <spences10apps@gmail.com>
2023-01-23 13:27:04 +0000

    refactor: :recycle: use action for node transformation

--- src/lib/components/sarcasm.svelte
+++ src/lib/components/sarcasm.svelte
@@ -1,11 +1,16 @@
 <script>
-  export let children = ''
-  let sarky = children
-    .split('')
-    .map((char, i) => char[`to${i % 2 ? 'Upper' : 'Lower'}Case`]())
-    .join('')
+  let children = ''
+
+  const sarky = node => {
+    children = node.childNodes[0].nodeValue
+
+    node.childNodes[0].nodeValue = children
+      .split('')
+      .map((char, i) => char[`to${i % 2 ? 'Upper' : 'Lower'}Case`]())
+      .join('')
+  }
 </script>
 
-<span class="font-semibold">
+<span class="font-semibold" use:sarky>
   <slot />
 </span>
