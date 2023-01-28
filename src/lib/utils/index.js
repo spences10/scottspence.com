251f80cbb3a11d4a9a6b5bd9fd0502bfd0a27dc2
spences10 <spences10apps@gmail.com>
2023-01-28 17:32:31 +0000

    refactor: :recycle: new utils

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -10,9 +10,27 @@ export const getPadding = aspectRatio => {
   return config[aspectRatio]
 }
 
-export const objectToQueryParams = obj => {
+export const object_to_query_params = obj => {
   const params = Object.entries(obj).map(
     ([key, value]) => `${key}=${value}`
   )
   return '?' + params.join('&')
+}
+
+// https://stackoverflow.com/a/50836512
+export const number_crunch = value => {
+  if (value >= 1000000) {
+    value = (value / 1000000).toFixed + 'm'
+  } else if (value >= 1000) {
+    value = (value / 1000).toFixed(2) + 'k'
+  }
+  return value
+}
+
+// https://stackoverflow.com/a/46545530
+export const shuffle_array = array => {
+  return array
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
 }
