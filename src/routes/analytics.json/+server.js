0791419a3a635ab47f109e647efe89505288d436
spences10 <spences10apps@gmail.com>
2023-01-28 17:33:22 +0000

    refactor: :recycle: defaults removed for no date params

--- src/routes/analytics.json/+server.js
+++ src/routes/analytics.json/+server.js
@@ -1,23 +1,24 @@
 import { FATHOM_API_KEY } from '$env/static/private'
 import { PUBLIC_FATHOM_ID } from '$env/static/public'
-import { objectToQueryParams } from '@lib/utils'
+import { object_to_query_params } from '@lib/utils'
 import { json } from '@sveltejs/kit'
 
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ url }) => {
   const pathname = url.searchParams.get('pathname') ?? '/'
-  const date_grouping = url.searchParams.get('date_grouping') ?? 'day'
-  const date_from =
-    url.searchParams.get('date_from') ?? '2023-01-20 00:00:00'
-  const date_to =
-    url.searchParams.get('date_to') ?? '2023-01-27 23:59:59'
-  const sort_by = url.searchParams.get('sort_by') ?? 'timestamp:desc'
+  const date_grouping = url.searchParams.get('date_grouping') ?? null // 'hour', 'day', 'month', 'year'
+  const date_from = url.searchParams.get('date_from') ?? null
+  const date_to = url.searchParams.get('date_to') ?? null
+  // date_from = '2023-01-20 00:00:00'
+  // date_to = '2023-01-27 23:59:59'
+  const sort_by = url.searchParams.get('sort_by') ?? null // 'timestamp:desc'
 
+  // https://stackoverflow.com/a/51200448
   const date_params = {
-    date_grouping,
-    date_from,
-    date_to,
-    sort_by,
+    ...(date_from && { date_from }),
+    ...(date_to && { date_to }),
+    ...(date_grouping && { date_grouping }),
+    ...(sort_by && { sort_by }),
   }
 
   const default_params = {
@@ -34,7 +35,7 @@ export const GET = async ({ url }) => {
     const headers_auth = new Headers()
     headers_auth.append(`Authorization`, `Bearer ${FATHOM_API_KEY}`)
     const res = await fetch(
-      `https://api.usefathom.com/v1/aggregations${objectToQueryParams(
+      `https://api.usefathom.com/v1/aggregations${object_to_query_params(
         params
       )}`,
       {
