d549dc3c81de81abc04c5fd7c7122477862e7424
spences10 <spences10apps@gmail.com>
2023-01-30 20:20:56 +0000

    refactor: :recycle: refactor base path, export all analytics

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,47 +1,37 @@
 import {
   endOfDay,
   endOfMonth,
-  endOfWeek,
+  endOfYear,
   startOfDay,
   startOfMonth,
-  startOfWeek,
+  startOfYear,
 } from 'date-fns'
 
 export const load = async ({ fetch, params }) => {
   const { slug } = params
-  
-  const week_start_date = startOfWeek(new Date(), {
-    weekStartsOn: 1,
-  }).toISOString()
-  const week_end_date = endOfWeek(new Date(), {
-    weekStartsOn: 1,
-  }).toISOString()
+  const base_path = `../analytics.json?pathname=/posts/${slug}`
 
   const day_start = startOfDay(new Date()).toISOString()
   const day_end = endOfDay(new Date()).toISOString()
 
   const month_start = startOfMonth(new Date()).toISOString()
   const month_end = endOfMonth(new Date()).toISOString()
-  console.log('=====================')
-  console.log(`week starts on`, week_start_date)
-  console.log(`week ends on`, week_end_date)
-  console.log(`day start`, day_start)
-  console.log(`day end`, day_end)
-  console.log(`month start`, month_start)
-  console.log(`month end`, month_end)
-  console.log('=====================')
+
+  const year_start = startOfYear(new Date()).toISOString()
+  const year_end = endOfYear(new Date()).toISOString()
+
   // get hourly visits
-  // const fetch_hourly_visits = async () => {
-  //   const res = await fetch(
-  //     `../analytics.json?pathname=/posts/${slug}&date_from=${week_start_date.toISOString()}&date_to=${week_end_date.toISOString()}`
-  //   )
-  //   const { analytics } = await res.json()
-  //   return analytics
-  // }
+  const fetch_hourly_visits = async () => {
+    const res = await fetch(
+      `${base_path}&date_from=${day_start}&date_to=${day_end}&date_grouping=hour&sort_by=timestamp:asc`
+    )
+    const { analytics } = await res.json()
+    return analytics
+  }
   // get daily visits
   const fetch_daily_visits = async () => {
     const res = await fetch(
-      `../analytics.json?pathname=/posts/${slug}&date_from=${day_start}&date_to=${day_end}`
+      `${base_path}&date_from=${day_start}&date_to=${day_end}`
     )
     const { analytics } = await res.json()
     return analytics
@@ -49,7 +39,7 @@ export const load = async ({ fetch, params }) => {
   // get monthly visits
   const fetch_monthly_visits = async () => {
     const res = await fetch(
-      `../analytics.json?pathname=/posts/${slug}`
+      `${base_path}&date_from=${month_start}&date_to=${month_end}&date_grouping=month`
     )
     const { analytics } = await res.json()
     return analytics
@@ -57,16 +47,16 @@ export const load = async ({ fetch, params }) => {
   // get yearly visits
   const fetch_yearly_visits = async () => {
     const res = await fetch(
-      `../analytics.json?pathname=/posts/${slug}`
+      `${base_path}&date_from=${year_start}&date_to=${year_end}&date_grouping=year`
     )
     const { analytics } = await res.json()
     return analytics
   }
 
   return {
-    // hourly_visits: fetch_hourly_visits(),
+    hourly_visits: fetch_hourly_visits(),
     daily_visits: fetch_daily_visits(),
-    // monthly_visits: fetch_monthly_visits(),
-    // yearly_visits: fetch_yearly_visits(),
+    monthly_visits: fetch_monthly_visits(),
+    yearly_visits: fetch_yearly_visits(),
   }
 }
