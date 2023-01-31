ba23f869667252439dff5b7ffb5772fc3351cafd
spences10 <spences10apps@gmail.com>
2023-01-31 09:53:57 +0000

    refactor: :recycle: return empty object if no one is looking

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -39,5 +39,11 @@ export const get_current_page_visitors = (path, content) => {
   let current_visitors = content.find(
     visitor => visitor.pathname === path
   )
+  if (!current_visitors)
+    return (current_visitors = {
+      total: 0,
+      content: [],
+      referrers: [],
+    })
   return current_visitors
 }
