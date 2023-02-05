feb932e8c818d6a9276badb4895602473c914fd6
spences10 <spences10apps@gmail.com>
2023-02-05 12:44:37 +0000

    fix: :bug: fix redirect

--- src/routes/[yyyy]/[mm]/[dd]/[slug]/+server.js
+++ src/routes/[yyyy]/[mm]/[dd]/[slug]/+server.js
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit'
 
-export const load = async ({ params }) => {
+export const GET = async ({ params }) => {
   const { slug } = params
   throw redirect(301, `/posts/${slug}`)
 }
