e8c716994b311fed6e3777ab24976a4bdc13687b
spences10 <spences10apps@gmail.com>
2023-03-14 21:06:52 +0000

    refactor: :lipstick: mobile classes

--- src/lib/components/back-to-top.svelte
+++ src/lib/components/back-to-top.svelte
@@ -21,7 +21,7 @@
   <button
     on:click={scroll_to_top}
     transition:fly={{ y: 200, duration: 300 }}
-    class="fixed bottom-8 right-8 btn btn-secondary normal-case shadow-2xl focus:outline-none focus:ring-2 focus:ring-accent"
+    class="fixed bottom-20 lg:bottom-8 right-4 lg:right-8 btn btn-secondary normal-case shadow-2xl focus:outline-none focus:ring-2 focus:ring-accent"
     aria-label="Back to top"
   >
     Back to top
