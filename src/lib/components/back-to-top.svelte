9985be60f7c6b411022ed1fefd08907233a4d95f
spences10 <spences10apps@gmail.com>
2023-03-14 20:40:54 +0000

    refactor: :recycle: add aria button label, rename functions 🐍

--- src/lib/components/back-to-top.svelte
+++ src/lib/components/back-to-top.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition'
 
-	let showScrollButton = false
+	let show_scroll_button = false
 
-	function scrollToTop() {
+	function scroll_to_top() {
 		window.scrollTo({
 			top: 0,
 			behavior: 'smooth',
 		})
 	}
 
-	function handleScroll() {
-		showScrollButton = window.pageYOffset > 0
+	function handle_scroll() {
+		show_scroll_button = window.pageYOffset > 0
 	}
 </script>
 
-<svelte:window on:scroll={handleScroll} />
+<svelte:window on:scroll={handle_scroll} />
 
-{#if showScrollButton}
+{#if show_scroll_button}
 	<button
-		on:click={scrollToTop}
+		on:click={scroll_to_top}
 		transition:fly={{ y: 200, duration: 300 }}
-		class="fixed bottom-8 right-8 btn btn-secondary normal-case shadow-2xl"
+		class="fixed bottom-8 right-8 btn btn-secondary normal-case shadow-2xl focus:outline-none focus:ring-2 focus:ring-accent"
+		aria-label="Back to top"
 	>
 		Back to top
 	</button>
