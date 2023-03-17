00e7fb7cb052e4e3728fd35ffc80def0cf4f2ae1
spences10 <spences10apps@gmail.com>
2023-03-17 15:36:05 +0000

    ci: :construction_worker: now with playwright projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,20 @@ const config: PlaywrightTestConfig = {
     port: 4173,
   },
   testDir: 'tests',
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
 }
 
 export default config
