b55dded15fa2fb30afd8be04a374343c8a184587
spences10 <spences10apps@gmail.com>
2023-03-17 13:21:21 +0000

    refactor: :recycle: add data-testid

--- src/lib/components/back-to-top.svelte
+++ src/lib/components/back-to-top.svelte
@@ -27,6 +27,7 @@
     transition:fly={{ y: 200, duration: 300 }}
     class="fixed bottom-20 lg:bottom-8 right-4 lg:right-8 btn btn-secondary normal-case shadow-2xl focus:outline-none focus:ring-2 focus:ring-accent"
     aria-label="Back to top"
+    data-testid="back-to-top"
   >
     Back to top
   </button>
