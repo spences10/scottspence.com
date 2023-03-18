63fab0de181bac7b792a176dfe683c4f390dc4e0
spences10 <spences10apps@gmail.com>
2023-03-18 18:43:07 +0000

    refactor: :construction: do some heatmap work

--- src/routes/heatmap/+page.svelte
+++ src/routes/heatmap/+page.svelte
@@ -1,1 +1,54 @@
-<!--  -->
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  export let data: PageData
+
+  // Function to calculate the color based on the value
+  function getColor(value: number) {
+    // You can customize this function to suit your color scale
+    const colors = [
+      '#ebedf0',
+      '#9be9a8',
+      '#40c463',
+      '#30a14e',
+      '#216e39',
+    ]
+    return colors[Math.min(value, colors.length - 1)]
+  }
+</script>
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
+
+<div class="heatmap">
+  {#each data as week (week[0].date)}
+    <div class="week">
+      {#each week as day}
+        <div
+          class="day"
+          style="background-color: {getColor(day.value)}"
+          title="{day.date}: {day.value}"
+        ></div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .heatmap {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(12px, 1fr));
+    gap: 2px;
+  }
+
+  .week {
+    display: grid;
+    grid-template-columns: repeat(7, auto);
+    gap: 2px;
+  }
+
+  .day {
+    width: 12px;
+    height: 12px;
+    border-radius: 2px;
+  }
+</style>
--- src/routes/heatmap/+page.ts
+++ src/routes/heatmap/+page.ts
@@ -1,11 +1,79 @@
+import { endOfYear, startOfYear } from 'date-fns'
 import type { PageLoad } from './$types'
 
-// export const prerender = true
+interface Post {
+  date: string
+}
+
+interface GroupedPosts {
+  [key: string]: number
+}
+
 export const load: PageLoad = async ({ fetch }) => {
-  const year = 2019
-  const res = await fetch(`/github-contributions.json?year=${year}`)
-  if (res.ok) {
-    const data = await res.json()
-    return { data }
+  const response = await fetch('/posts.json')
+  const posts: Post[] = await response.json()
+  const grouped_posts: GroupedPosts = group_posts_by_date(posts)
+
+  const year_start = startOfYear(new Date())
+  const year_end = endOfYear(new Date())
+
+  const data = generate_heatmap_data(
+    grouped_posts,
+    year_start,
+    year_end
+  )
+
+  return {
+    data,
   }
+}
+
+function group_posts_by_date(posts: Post[]) {
+  const grouped_posts: GroupedPosts = {}
+
+  posts.forEach(post => {
+    const date = new Date(post.date)
+    const date_string = date.toISOString().slice(0, 10)
+
+    if (grouped_posts[date_string]) {
+      grouped_posts[date_string]++
+    } else {
+      grouped_posts[date_string] = 1
+    }
+  })
+
+  return grouped_posts
+}
+
+function generate_heatmap_data(
+  grouped_posts: GroupedPosts,
+  year_start: Date,
+  year_end: Date
+) {
+  const heatmap_data = []
+  let current_week = []
+  let currentDate = new Date(year_start)
+
+  while (currentDate <= year_end) {
+    const day_of_week = currentDate.getDay()
+    const date_string = currentDate.toISOString().slice(0, 10)
+
+    current_week[day_of_week] = {
+      date: date_string,
+      value: grouped_posts[date_string] || 0,
+    }
+
+    if (day_of_week === 6) {
+      heatmap_data.push(current_week)
+      current_week = []
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1)
+  }
+
+  if (current_week.length > 0) {
+    heatmap_data.push(current_week)
+  }
+
+  return heatmap_data
 }
