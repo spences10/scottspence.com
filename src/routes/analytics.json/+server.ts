8873e80d5b1b100909867665bcecbffa5e63b49e
spences10 <spences10apps@gmail.com>
2023-03-19 12:52:59 +0000

    refactor: :recycle: abstract out functions

--- src/routes/analytics.json/+server.ts
+++ src/routes/analytics.json/+server.ts
@@ -6,50 +6,82 @@ import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async ({ url }) => {
   const pathname = url.searchParams.get('pathname') ?? '/'
-  const date_grouping = url.searchParams.get('date_grouping') ?? null // 'hour', 'day', 'month', 'year'
-  const date_from = url.searchParams.get('date_from') ?? null
-  const date_to = url.searchParams.get('date_to') ?? null
-  // date_from = '2023-01-20 00:00:00'
-  // date_to = '2023-01-27 23:59:59'
-  const sort_by = url.searchParams.get('sort_by') ?? null // 'timestamp:desc'
+  const date_grouping = url.searchParams.get('date_grouping')
+  const date_from = url.searchParams.get('date_from')
+  const date_to = url.searchParams.get('date_to')
+  const sort_by = url.searchParams.get('sort_by')
 
-  // https://stackoverflow.com/a/51200448
-  const date_params = {
-    ...(date_from && { date_from }),
-    ...(date_to && { date_to }),
-    ...(date_grouping && { date_grouping }),
-    ...(sort_by && { sort_by }),
-  }
-
-  const default_params = {
-    entity: 'pageview',
-    entity_id: PUBLIC_FATHOM_ID,
-    aggregates: 'visits,uniques,pageviews,avg_duration,bounce_rate',
-    field_grouping: 'pathname',
-    filters: `[{"property": "pathname","operator": "is","value": "${pathname}"}]`,
-  }
-
+  const date_params = build_date_params(
+    date_from,
+    date_to,
+    date_grouping,
+    sort_by
+  )
+  const default_params = build_default_params(pathname)
   const params = { ...default_params, ...date_params }
 
   try {
     const headers_auth = new Headers()
-    headers_auth.append(`Authorization`, `Bearer ${FATHOM_API_KEY}`)
-    const res = await fetch(
-      `https://api.usefathom.com/v1/aggregations${object_to_query_params(
-        params
-      )}`,
-      {
-        headers: headers_auth,
-      }
+    headers_auth.append('Authorization', `Bearer ${FATHOM_API_KEY}`)
+
+    const analytics_data = await fetch_analytics_data(
+      params,
+      headers_auth
     )
 
     return json({
-      analytics: await res.json(),
+      analytics: analytics_data,
     })
   } catch (error) {
+    if (error instanceof Error) {
+      return json({
+        error: 'An error occurred: ' + error.message,
+        status: 500,
+      })
+    }
     return json({
-      error: 'Big oof! Sorry' + error,
+      error: 'An unknown error occurred',
       status: 500,
     })
   }
+}
+
+const build_date_params = (
+  date_from: string | null,
+  date_to: string | null,
+  date_grouping: string | null,
+  sort_by: string | null
+) => ({
+  ...(date_from && { date_from }),
+  ...(date_to && { date_to }),
+  ...(date_grouping && { date_grouping }),
+  ...(sort_by && { sort_by }),
+})
+
+const build_default_params = (pathname: string) => ({
+  entity: 'pageview',
+  entity_id: PUBLIC_FATHOM_ID,
+  aggregates: 'visits,uniques,pageviews,avg_duration,bounce_rate',
+  field_grouping: 'pathname',
+  filters: `[{"property": "pathname","operator": "is","value": "${pathname}"}]`,
+})
+
+const fetch_analytics_data = async (
+  params: { [s: string]: unknown } | ArrayLike<unknown>,
+  headers: Headers
+) => {
+  const res = await fetch(
+    `https://api.usefathom.com/v1/aggregations${object_to_query_params(
+      params
+    )}`,
+    {
+      headers,
+    }
+  )
+
+  if (!res.ok) {
+    throw new Error(`HTTP error ${res.status}: ${res.statusText}`)
+  }
+
+  return await res.json()
 }
