d678e2eb4531e8aac063e932a85adaf1c2d1bb82
spences10 <spences10apps@gmail.com>
2023-04-10 10:51:45 +0100

    refactor: :recycle: remove previous headings

--- src/lib/utils/get-headings.ts
+++ src/lib/utils/get-headings.ts
@@ -2,10 +2,16 @@ import { tick } from 'svelte'
 
 export const get_headings = async (headings: string = 'h2') => {
   await tick()
+
+  // Clear previous headings
+  const previousHeadings = document.querySelectorAll('.toc-heading')
+  previousHeadings.forEach(heading => heading.remove())
+
   const headingElements = document.querySelectorAll(headings)
   return Array.from(headingElements).map((element, index) => {
     const href = element.id || `heading-${index}`
     element.id = href
+    element.classList.add('toc-heading')
     return { label: element.textContent || '', href: `#${href}` }
   })
 }
