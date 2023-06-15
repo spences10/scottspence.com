c112b5fdf970eea1772db23a44898a288dc509c3
spences10 <spences10apps@gmail.com>
2023-06-15 22:25:23 +0100

    test: :white_check_mark: update tests for component refactor

--- src/lib/components/stats-card.test.ts
+++ src/lib/components/stats-card.test.ts
@@ -6,29 +6,31 @@ describe('StatsCard', () => {
   afterEach(cleanup)
 
   function renderStatsCard(
-    stats: { visits: number; uniques: number; pageviews: number },
-    title: string,
-    time_period: string
+    daily_visits: AnalyticsData | null | undefined,
+    monthly_visits: AnalyticsData | null | undefined,
+    yearly_visits: AnalyticsData | null | undefined
   ) {
     return render(StatsCard, {
       props: {
-        stats,
-        title,
-        time_period,
+        daily_visits,
+        monthly_visits,
+        yearly_visits,
       },
     })
   }
 
   it('should render stats correctly', () => {
-    const stats = {
-      visits: 100,
-      uniques: 50,
-      pageviews: 200,
+    const daily_visits = {
+      visits: '100',
+      uniques: '50',
+      pageviews: '200',
+      avg_duration: null,
+      bounce_rate: 0.2,
+      date: '2023-06-15',
+      pathname: '/',
     }
-    const title = 'Stats Title'
-    const time_period = 'day'
 
-    const { getByText } = renderStatsCard(stats, title, time_period)
+    const { getByText } = renderStatsCard(daily_visits, null, null)
 
     expect(getByText('Entries Today')).toBeTruthy()
     expect(getByText('100')).toBeTruthy()
@@ -38,41 +40,65 @@ describe('StatsCard', () => {
     expect(getByText('200')).toBeTruthy()
   })
 
-  it('should render 0 for null values', () => {
-    const stats = {
-      visits: 0,
-      uniques: 50,
-      pageviews: 200,
-    }
-    const title = 'Stats Title'
-    const time_period = 'day'
-
-    const { getByText } = renderStatsCard(stats, title, time_period)
-
-    expect(getByText('0')).toBeTruthy()
-  })
-
   it('should render stats for different time periods', () => {
-    const stats = {
-      visits: 1000,
-      uniques: 500,
-      pageviews: 2000,
-    }
-    const title = 'Stats Title'
-
-    const time_periods = {
-      day: 'Today',
-      week: 'This Week',
-      month: 'This Month',
-      year: 'This Year',
+    const daily_visits = {
+      visits: '100',
+      uniques: '50',
+      pageviews: '200',
+      avg_duration: null,
+      bounce_rate: 0.2,
+      date: '2023-06-15',
+      pathname: '/',
     }
 
-    Object.entries(time_periods).forEach(([time_period, label]) => {
-      const { getByText } = renderStatsCard(stats, title, time_period)
+    const monthly_visits = {
+      visits: '1000',
+      uniques: '500',
+      pageviews: '2000',
+      avg_duration: null,
+      bounce_rate: 0.2,
+      date: '2023-06-01',
+      pathname: '/',
+    }
 
-      expect(getByText(`Entries ${label}`)).toBeTruthy()
-      expect(getByText(`Visitors ${label}`)).toBeTruthy()
-      expect(getByText(`Views ${label}`)).toBeTruthy()
-    })
+    const yearly_visits = {
+      visits: '10000',
+      uniques: '5000',
+      pageviews: '20000',
+      avg_duration: null,
+      bounce_rate: 0.2,
+      date: '2023-01-01',
+      pathname: '/',
+    }
+
+    const { getByText } = renderStatsCard(
+      daily_visits,
+      monthly_visits,
+      yearly_visits
+    )
+
+    // For daily visits
+    expect(getByText('Entries Today')).toBeTruthy()
+    expect(getByText('100')).toBeTruthy()
+    expect(getByText('Visitors Today')).toBeTruthy()
+    expect(getByText('50')).toBeTruthy()
+    expect(getByText('Views Today')).toBeTruthy()
+    expect(getByText('200')).toBeTruthy()
+
+    // For monthly visits
+    expect(getByText('Entries This Month')).toBeTruthy()
+    expect(getByText('1k')).toBeTruthy()
+    expect(getByText('Visitors This Month')).toBeTruthy()
+    expect(getByText('500')).toBeTruthy()
+    expect(getByText('Views This Month')).toBeTruthy()
+    expect(getByText('2k')).toBeTruthy()
+
+    // For yearly visits
+    expect(getByText('Entries This Year')).toBeTruthy()
+    expect(getByText('10k')).toBeTruthy()
+    expect(getByText('Visitors This Year')).toBeTruthy()
+    expect(getByText('5k')).toBeTruthy()
+    expect(getByText('Views This Year')).toBeTruthy()
+    expect(getByText('20k')).toBeTruthy()
   })
 })
