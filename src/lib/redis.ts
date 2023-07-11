5c9d3b0705281b8b77f25059ac9930d30c491595
spences10 <spences10apps@gmail.com>
2023-07-11 16:00:12 +0100

    refactor: :recycle: normalise url slug!

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,16 @@
 import { REDIS_CONNECTION, VISITORS_KEY } from '$env/static/private'
 import Redis from 'ioredis'
 
+export function normalise_slug(slug: string): string {
+  return slug.toLowerCase().replace(/\s+/g, '-')
+}
+
 export function current_visitors_key(): string {
   return `visitors:${VISITORS_KEY}`
 }
 
 export function page_analytics_key(slug: string): string {
-  return `slug:${slug}`
+  return `slug:${normalise_slug(slug)}`
 }
 
 export default REDIS_CONNECTION
