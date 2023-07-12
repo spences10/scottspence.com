a4358d75ed1953dbb5795c2e1a45c08915267e28
spences10 <spences10apps@gmail.com>
2023-07-12 20:45:18 +0100

    chore: :arrow_up: update deps
    remove multi browser for now, webkit derp

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,23 +4,23 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'pnpm run build && pnpm run preview',
     port: 4173,
-    timeout: 2 * 60 * 1000,
+    timeout: 3 * 60 * 1000,
   },
   testDir: 'tests',
-  projects: [
-    {
-      name: 'Chromium',
-      use: { browserName: 'chromium' },
-    },
-    {
-      name: 'Firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'WebKit',
-      use: { browserName: 'webkit' },
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: 'Chromium',
+  //     use: { browserName: 'chromium' },
+  //   },
+  //   {
+  //     name: 'Firefox',
+  //     use: { browserName: 'firefox' },
+  //   },
+  //   {
+  //     name: 'WebKit',
+  //     use: { browserName: 'webkit' },
+  //   },
+  // ],
 }
 
 export default config
