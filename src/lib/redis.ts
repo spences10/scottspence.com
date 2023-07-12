771abaae2f1e6193cc4a8fcc891d543c6adde38b
spences10 <spences10apps@gmail.com>
2023-07-12 20:01:33 +0100

    ref

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,16 +1,13 @@
 import { REDIS_CONNECTION, VISITORS_KEY } from '$env/static/private'
 import Redis from 'ioredis'
 
-export function normalise_slug(slug: string): string {
-  return slug.toLowerCase().replace(/\s+/g, '-')
-}
-
 export function current_visitors_key(): string {
   return `visitors:${VISITORS_KEY}`
 }
 
 export function page_analytics_key(slug: string): string {
-  return `slug:${normalise_slug(slug)}`
+  const encoded_slug = encodeURIComponent(slug)
+  return `slug:${encoded_slug}`
 }
 
 export default REDIS_CONNECTION
