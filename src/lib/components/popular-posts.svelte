9e761331558434940994b9fb497e931fc86cd23b
spences10 <spences10apps@gmail.com>
2023-07-23 20:17:53 +0100

    refactor: :recycle: add conditional if analytics are off

--- src/lib/components/popular-posts.svelte
+++ src/lib/components/popular-posts.svelte
@@ -16,40 +16,45 @@
   }
 </script>
 
-<div class="m-0 mb-20 sm:-mx-30 lg:-mx-40">
-  <p class="text-xl mb-4">
-    Take a look at some popular content from me...
-  </p>
-
-  <select bind:value={selected_period} class="select">
-    <option value="popular_posts_daily">Views today</option>
-    <option value="popular_posts_monthly">Views this month</option>
-    <option value="popular_posts_yearly">Views this year</option>
-  </select>
-
-  <div
-    class="grid gap-4 grid-cols-1 relative md:grid-cols-2 lg:grid-cols-4"
-  >
-    {#each posts as post}
-      <a
-        data-sveltekit-reload
-        href={$page.url.origin + post.pathname}
-        on:click={() => Fathom.trackGoal(`WKHRXHV8`, 0)}
-        class="h-full"
+{#if posts.length}
+  <div class="m-0 mb-20 sm:-mx-30 lg:-mx-40">
+    <p class="text-xl mb-8">
+      Take a look at some popular content from me...
+      <select
+        bind:value={selected_period}
+        class="select select-sm border border-secondary"
       >
-        <aside
-          class="rounded-box font-bold h-full p-5 transition card shadow-lg hover:text-accent-focus border border-secondary"
+        <option value="popular_posts_daily">Views today</option>
+        <option value="popular_posts_monthly">Views this month</option
         >
-          <h3 class="mb-5 text-2xl">
-            {post.title}
-          </h3>
-          <div class="mt-5">
-            <span class="text-primary mb-4 bottom-0 absolute">
-              Views: {number_crunch(post.pageviews)}
-            </span>
-          </div>
-        </aside>
-      </a>
-    {/each}
+        <option value="popular_posts_yearly">Views this year</option>
+      </select>
+    </p>
+
+    <div
+      class="grid gap-4 grid-cols-1 relative md:grid-cols-2 lg:grid-cols-4"
+    >
+      {#each posts as post}
+        <a
+          data-sveltekit-reload
+          href={$page.url.origin + post.pathname}
+          on:click={() => Fathom.trackGoal(`WKHRXHV8`, 0)}
+          class="h-full"
+        >
+          <aside
+            class="rounded-box font-bold h-full p-5 transition card shadow-lg hover:text-accent-focus border border-secondary"
+          >
+            <h3 class="mb-5 text-2xl">
+              {post.title}
+            </h3>
+            <div class="mt-5">
+              <span class="text-primary mb-4 bottom-0 absolute">
+                Views: {number_crunch(post.pageviews)}
+              </span>
+            </div>
+          </aside>
+        </a>
+      {/each}
+    </div>
   </div>
-</div>
+{/if}
