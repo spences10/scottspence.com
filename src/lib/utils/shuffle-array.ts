9e1b140c59c331aef42391479af45f4063f6b0c3
spences10 <spences10apps@gmail.com>
2023-08-01 17:54:44 +0100

    chore: :coffin: remove unused shuffle function

--- src/lib/utils/shuffle-array.ts
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/a/46545530
-export const shuffle_array = (array: any[]) => {
-  return array
-    .map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value)
-}
