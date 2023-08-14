07a6e82742fcfe1be2757af724b7f1c648b4790d
spences10 <spences10apps@gmail.com>
2023-08-14 12:07:05 +0100

    refactor: :bento: remove unused assets

--- src/lib/assets/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ButtButt } from './butt.png'
-export { default as ScottFace } from './scott-face.jpg'
-export { default as ScottMugFace } from './scott-mug-face.png'
-export { default as Sparkles } from './sparkles.png'
-export { default as TracedMugFace } from './traced-svg-scott-mug-face.svg'
