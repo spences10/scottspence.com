92bb05a672c405607f517a5b41b0d25bbc076bcc
spences10 <spences10apps@gmail.com>
2023-09-30 14:15:51 +0100

    refactor: :recycle: first pass on using buttondown

--- src/lib/components/newsletter-signup.svelte
+++ src/lib/components/newsletter-signup.svelte
@@ -1,44 +1,47 @@
 <script lang="ts">
+  import { enhance } from '$app/forms'
   import { scale_and_fade } from '$lib/utils'
+  import type { ActionResult } from '@sveltejs/kit'
   import * as Fathom from 'fathom-client'
+  import { writable } from 'svelte/store'
 
   let email = ''
   let success = false
   let responseMessage: string[] = []
 
-  const submitForm = async () => {
-    const submit = await fetch('/submit-email.json', {
-      method: 'POST',
-      body: JSON.stringify({ email }),
-    })
-    const data = await submit.json()
+  let button_disabled = writable(false)
+  let action_result: ActionResult
 
-    if (data.message === 'bad request') {
-      success = true
-      responseMessage = [
-        `Looks like you might already be signed up!`,
-        `Thanks for showing interest in my content though!`,
-        `If you haven't signed up already <a class="link" href="mailto:spences10apps@gmail.com">reach out</a> and let me know. 🙏`,
-      ]
-    }
-    if (data.message === 'email sent!') {
+  const handle_result = (result: ActionResult) => {
+    action_result = result
+    if (result.type === 'success') {
       success = true
       responseMessage = [
         `Awesome! 🥳`,
         `Thanks for showing interest in my content.`,
         `Please check your email to confirm your subscription. 📨`,
       ]
-    }
-    if (
-      data.message === 'something went wrong with the email submit!'
-    ) {
-      success = false
-      // TODO deal with failed response from server
+    } else if (result.type === 'failure') {
+      $button_disabled = true
+      if (result?.data?.body?.code === 'email_already_exists') {
+        responseMessage = [
+          `Looks like you might already be signed up!`,
+          `Thanks for showing interest in my content though!`,
+          `If you haven't signed up already <a class="link" href="mailto:derpemailsignup@scottspence.com">reach out</a> and let me know. 🙏`,
+        ]
+      } else {
+        responseMessage = [
+          `Oops! Something went wrong.`,
+          `Please try again later.`,
+        ]
+      }
+      setTimeout(() => {
+        $button_disabled = false
+      }, result?.data?.time_remaining * 1000)
     }
   }
 </script>
 
-<!-- TODO deal with failed response from server -->
 <div class="m-0 -mx-30 mb-10 max-h-96 lg:-mx-40">
   {#if success}
     <div
@@ -61,6 +64,26 @@
         </div>
       </div>
     </div>
+  {:else if action_result?.type === 'failure'}
+    <div class="mx-auto text-center max-w-7xl py-12 lg:py-16 lg:px-8">
+      <div
+        class="bg-primary rounded-box py-10 lg:flex lg:p-20 lg:items-center"
+      >
+        <div class="text-primary-content lg:flex-1 lg:w-0">
+          <h3 class=" font-extrabold tracking-tight text-3xl">
+            {responseMessage[0]}
+          </h3>
+          <p class="mt-4 text-lg">
+            {responseMessage[1]}
+          </p>
+          {#if responseMessage[2]}
+            <p class="mt-4 text-lg">
+              {@html responseMessage[2]}
+            </p>
+          {/if}
+        </div>
+      </div>
+    </div>
   {:else}
     <div
       out:scale_and_fade|global={{ delay: 200, duration: 400 }}
@@ -79,7 +102,17 @@
         </div>
         <div class="max-w-md mt-12 w-full lg:flex-1 lg:mt-0 lg:ml-8">
           <div class="form-control">
-            <form class="" on:submit|preventDefault={submitForm}>
+            <form
+              method="POST"
+              class=""
+              action="/submit-email"
+              use:enhance={() => {
+                return ({ update, result }) => {
+                  handle_result(result)
+                  update({ reset: true })
+                }
+              }}
+            >
               <label for="email" class="label">
                 <span class="sr-only label-text">Your Email</span>
               </label>
@@ -104,6 +137,7 @@
                     Fathom.trackGoal(`ZWGL5VLX`, 0)
                   }}
                   value="sign me up!"
+                  disabled={$button_disabled}
                 />
               </div>
             </form>
