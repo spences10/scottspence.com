0965731819da102d209b142009d95122f7f677df
spences10 <spences10apps@gmail.com>
2023-10-07 20:18:12 +0100

    feat: :sparkles: set up page action with nodemailer

---
+++ src/routes/contact/+page.server.ts
@@ -0,0 +1,67 @@
+import {
+  EMAIL_APP_PASSWORD,
+  EMAIL_APP_USER,
+} from '$env/static/private'
+import { ratelimit } from '$lib/redis.js'
+import { fail } from '@sveltejs/kit'
+import nodemailer from 'nodemailer'
+
+export const actions = {
+  default: async ({ request, getClientAddress }) => {
+    const ip = getClientAddress()
+    const rate_limit_attempt = await ratelimit.limit(ip)
+
+    if (!rate_limit_attempt.success) {
+      const time_remaining = Math.floor(
+        (rate_limit_attempt.reset - new Date().getTime()) / 1000,
+      )
+
+      return fail(429, {
+        error: `Rate limit exceeded. Try again in ${time_remaining} seconds`,
+        time_remaining,
+      })
+    }
+
+    try {
+      const data = await request.formData()
+      const name = data.get('name')?.toString()
+      const email = data.get('email')?.toString()
+      const reason = data.get('reason')?.toString()
+      const message = data.get('message')?.toString()
+
+      // Create a transporter object using the nodemailer library
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.fastmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: EMAIL_APP_USER,
+          pass: EMAIL_APP_PASSWORD,
+        },
+      })
+
+      // Set up email data
+      const mail_options = {
+        from: `"${name}" <${email}>`,
+        to: 'contact@scottspence.com',
+        subject: reason,
+        text: message,
+      }
+
+      // Send email
+      const info = await transporter.sendMail(mail_options)
+
+      return {
+        status: 200,
+        body: {
+          message: 'Email sent successfully',
+          messageId: info.messageId,
+        },
+      }
+    } catch (error) {
+      return fail(500, {
+        error: 'Internal server error',
+      })
+    }
+  },
+}
