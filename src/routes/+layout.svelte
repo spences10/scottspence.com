60b180f9a7afaab2829b63ce9b8119b42ad31a87
spences10 <spences10apps@gmail.com>
2023-11-19 18:57:20 +0000

    feat: :sparkles: add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,11 @@
   import '../app.css'
   import '../prism.css'
 
+  import { dev } from '$app/environment'
+  import { inject } from '@vercel/analytics'
+
+  inject({ mode: dev ? 'development' : 'production' })
+
   export let data
 
   $popular_posts_store = data?.popular_posts
