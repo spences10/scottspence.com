0bd6c4cb8413ab760e97bab1bc5d56b2203268e9
spences10 <spences10apps@gmail.com>
2023-12-24 09:42:58 +0000

    refactor: :recycle: try serverless config

--- src/routes/api/post-tags/+server.ts
+++ src/routes/api/post-tags/+server.ts
@@ -1,6 +1,9 @@
 import { get_post_tags } from '$lib/post-tags'
+import type { ServerlessConfig } from '@sveltejs/adapter-vercel'
 import type { RequestHandler } from '@sveltejs/kit'
 import { json } from '@sveltejs/kit'
+
+export const config: ServerlessConfig = { runtime: 'nodejs18.x' }
 
 export const GET: RequestHandler = async () => {
   const { tags, posts_by_tag } = await get_post_tags()
