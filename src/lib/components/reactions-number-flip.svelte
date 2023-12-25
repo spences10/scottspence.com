8dc794f18fad116acf74ac7c9e4d33a301b9f901
spences10 <spences10apps@gmail.com>
2023-12-25 07:41:31 +0000

    refactor: :recycle: use runes

--- src/lib/components/reactions-number-flip.svelte
+++ src/lib/components/reactions-number-flip.svelte
@@ -6,30 +6,44 @@
   import { number_crunch } from '$lib/utils'
   import { spring } from 'svelte/motion'
 
-  export let count: number = 0
-  export let font_size = 'text-2xl'
-  export let emoji: string
-  export let value: string
-  export let disabled: boolean
-  export let aria_label: string
-
-  const displayed_count = spring(count)
-  $: displayed_count.set(count)
-  $: offset = modulo($displayed_count, 1)
-
-  function modulo(n: number, m: number) {
-    return ((n % m) + m) % m
-  }
+  let {
+    count,
+    font_size = 'text-2xl',
+    emoji,
+    value,
+    disabled,
+    aria_label,
+  } = $props<{
+    count: number
+    font_size?: string
+    emoji: string
+    value: string
+    disabled: boolean
+    aria_label: string
+  }>()
 
   let base_width = 2
   let padding = 3
   let character_width = 1
 
+  const displayed_count = spring(count)
+  let offset = $state(0)
+  let button_width = $state(``)
+
+  $effect(() => {
+    displayed_count.set(count)
+    offset = modulo($displayed_count, 1)
+    button_width =
+      base_width + padding + character_width * crunched_length + 'rem'
+  })
+
+  function modulo(n: number, m: number) {
+    return ((n % m) + m) % m
+  }
+
   // calculate initial button width
   let crunched_number = number_crunch(count)
   let crunched_length = crunched_number.length
-  let button_width =
-    base_width + padding + character_width * crunched_length + 'rem'
 
   function handle_click() {
     count += 1
@@ -46,7 +60,7 @@
   {value}
   {disabled}
   class="btn btn-primary overflow-hidden lowercase relative shadow-xl rounded-box"
-  on:click={handle_click}
+  onclick={handle_click}
   style:width={button_width}
   title={count > 1000 ? `${value} ${count}` : ''}
   aria-label={aria_label}
