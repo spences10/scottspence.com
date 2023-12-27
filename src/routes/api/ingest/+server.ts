53c1cc5da6f85037c02af7f9c47fbfc368f04fbd
spences10 <spences10apps@gmail.com>
2023-12-27 08:18:58 +0000

    refactor: :recycle: add auth token

--- src/routes/api/ingest/+server.ts
+++ src/routes/api/ingest/+server.ts
@@ -1,8 +1,27 @@
+import { env } from '$env/dynamic/private'
 import { json } from '@sveltejs/kit'
 
-// curl -X POST https://scottspence.com/api/ingest -H "Content-Type: application/json" -d '{}'
+// curl -X POST https://yourdomain.com/api/ingest \
+// -H "Content-Type: application/json" \
+// -d '{"token": "your-secret-token"}'
 
 export const POST = async ({ request }) => {
+  // Parse the request body as JSON
+  const body = await request.json()
+
+  // Extract the token from the request body
+  const token = body.token
+
+  // Check if the provided token matches your secret token
+  if (!token || token !== env.INGEST_TOKEN) {
+    return json({ message: 'Unauthorized' }, { status: 401 })
+  }
+
+  // Check if the provided token matches your secret token
+  if (token !== env.INGEST_TOKEN) {
+    return json({ message: 'Unauthorized' }, { status: 401 })
+  }
+  
   console.log('=====================')
   console.log({ request })
   console.log('=====================')
