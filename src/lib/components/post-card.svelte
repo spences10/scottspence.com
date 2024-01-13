8711ccca411397900a460d8a7b736657220bfc04
spences10 <spences10apps@gmail.com>
2024-01-13 16:50:57 +0000

    refactor: :recycle: rever Svelte 5 version
    move edge glow into post-card

--- src/lib/components/post-card.svelte
+++ src/lib/components/post-card.svelte
@@ -1,35 +1,39 @@
 <script lang="ts">
   import { differenceInDays, format } from 'date-fns'
-  import EdgeGlow from './edge-glow.svelte'
 
-  export let post: Post
+  const { post } = $props<{ post: Post }>()
 </script>
 
-<EdgeGlow>
-  <article
-    class="bg-base-100 mb-10 p-5 transition card hover:text-accent first:pt-0"
-  >
-    <a href={`/posts/${post.slug}`}>
-      <div>
-        <h2 class="font-black mb-1 text-3xl mt-5">
-          {post.title}
-        </h2>
-        <div class="mb-4 text-accent uppercase text-sm font-bold">
-          <time>{format(new Date(post.date), 'MMMM d, yyyy')}</time>
-          &bull;
-          <span>{post.reading_time_text}</span>
-          {#if differenceInDays(new Date(), new Date(post.date)) < 31}
-            <span
-              class="badge bg-primary hover:bg-secondary hover:text-secondary-content text-primary-content"
-            >
-              new
-            </span>
-          {/if}
+<div class="relative">
+  <div
+    class="absolute -inset-0 rounded-box bg-gradient-to-r from-primary to-secondary blur-sm"
+  />
+  <div class="relative">
+    <article
+      class="card mb-10 bg-base-100 p-5 transition first:pt-0 hover:text-accent"
+    >
+      <a href={`/posts/${post.slug}`}>
+        <div>
+          <h2 class="mb-1 mt-5 text-3xl font-black">
+            {post.title}
+          </h2>
+          <div class="mb-4 text-sm font-bold uppercase text-accent">
+            <time>{format(new Date(post.date), 'MMMM d, yyyy')}</time>
+            &bull;
+            <span>{post.reading_time_text}</span>
+            {#if differenceInDays(new Date(), new Date(post.date)) < 31}
+              <span
+                class="badge bg-primary text-primary-content hover:bg-secondary hover:text-secondary-content"
+              >
+                new
+              </span>
+            {/if}
+          </div>
         </div>
-      </div>
-      <div class="all-prose">
-        {@html post.preview_html}
-      </div>
-    </a>
-  </article>
-</EdgeGlow>
+        <div class="all-prose">
+          {@html post.preview_html}
+        </div>
+      </a>
+    </article>
+  </div>
+</div>
