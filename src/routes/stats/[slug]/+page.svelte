7fed88926cd11f4fbc2d8163ccf06af99bf4eab1
spences10 <spences10apps@gmail.com>
2024-01-26 20:49:40 +0000

    refactor: :recycle: make ahrefs happy

--- src/routes/stats/[slug]/+page.svelte
+++ src/routes/stats/[slug]/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-  import { StatsCard } from '$lib/components'
+  import { page } from '$app/stores'
+  import { Head, StatsCard } from '$lib/components'
+  import { name } from '$lib/info.js'
+  import { og_image_url } from '$lib/utils'
 
   const { data } = $props()
 </script>
+
+<Head
+  title={`Page stats for ${data.analytics.yearly_visits.slug}`}
+  description={`Pageview stats for ${data.analytics.yearly_visits.slug}`}
+  image={og_image_url(
+    name,
+    `scottspence.com`,
+    `Page stats for ${data.analytics.yearly_visits.slug}`,
+  )}
+  url={$page.url.toString()}
+/>
 
 <StatsCard
   daily_visits={data.analytics.daily_visits}
