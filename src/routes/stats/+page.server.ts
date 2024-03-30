69542450df0fdb4f8091328ef271485e2297bb92
spences10 <spences10apps@gmail.com>
2024-03-30 13:32:35 +0000

    refactor: :recycle: stats stuff

---
+++ src/routes/stats/+page.server.ts
@@ -0,0 +1,38 @@
+import { get_posts } from '$lib/posts'
+import { turso_client } from '$lib/turso'
+
+export let load = async () => {
+  const posts = await get_posts()
+
+  const client = turso_client()
+
+  const sql = `
+    SELECT 
+      DATE(timestamp) AS date,
+      SUM(views) AS total_page_views,
+      SUM(uniques) AS total_unique_visits
+    FROM 
+      analytics_pages
+    WHERE 
+      pathname = '/posts/use-chrome-in-ubuntu-wsl' AND 
+      strftime('%Y', timestamp) = '2021'
+    GROUP BY 
+      DATE(timestamp);
+  `
+
+  let analytics
+  try {
+    const result = await client.execute(sql)
+    // Convert the data to a plain object if necessary
+    analytics = result.rows.map(row => ({ ...row }))
+  } catch (error) {
+    console.error('Error fetching from Turso DB:', error)
+    return { posts, error: 'Error fetching analytics data' }
+  } finally {
+    if (client && typeof client.close === 'function') {
+      client.close()
+    }
+  }
+
+  return { posts, analytics }
+}
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+  let { data } = $props()
+  const { posts,analytics } = data
+</script>
+
+<pre>{JSON.stringify(analytics, null, 2)}</pre>
+
+<!-- {#each posts.posts as post, index}
+  <p>{post.title}</p>
+  <pre>{JSON.stringify(post, null, 2)}</pre>
+{/each} -->
+
+<pre>{JSON.stringify(posts, null, 2)}</pre>
 this is the stats page, but there's nothing here
