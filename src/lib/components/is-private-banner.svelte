2a7ba59d3a8ee9df8d58b3daf85be04502dbeeda
spences10 <spences10apps@gmail.com>
2025-04-18 19:59:56 +0100

    refactor: :recycle: daisyui classes workaround

--- src/lib/components/is-private-banner.svelte
+++ src/lib/components/is-private-banner.svelte
@@ -1,11 +1,7 @@
-<div class="alert mb-8">
-	<div class="flex w-full flex-col">
-		<p class="w-full text-center text-3xl font-medium">
-			This is a private post 🤫
-		</p>
-		<p class="text-center">
-			This means it's probably incomplete and not intended for public
-			consumption.
-		</p>
-	</div>
+<div class="alert mb-8 flex w-full flex-col items-center text-center">
+	<p class="w-full text-3xl font-medium">This is a private post 🤫</p>
+	<p class="w-full text-lg">
+		This means it's probably incomplete and not intended for public
+		consumption.
+	</p>
 </div>
