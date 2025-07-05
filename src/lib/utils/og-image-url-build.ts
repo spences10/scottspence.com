85efc893bb9755beeb98da43efe9b50e083d522b
spences10 <spences10apps@gmail.com>
2025-07-05 17:31:37 +0100

    fix: revert OG image URL to hardcoded endpoint for consistency

--- src/lib/utils/og-image-url-build.ts
+++ src/lib/utils/og-image-url-build.ts
@@ -1,4 +1,3 @@
-import { OG_ENDPOINT } from '$env/static/private'
 import { object_to_query_params } from './object-to-query-params'
 
 export const og_image_url = (
@@ -11,5 +10,5 @@ export const og_image_url = (
 		author,
 		website: website || `scottspence.com`,
 	}
-	return `${OG_ENDPOINT}${object_to_query_params(params)}`
+	return `https://scott.garden/og${object_to_query_params(params)}`
 }
