8c2e484905c78d39d7ed7478c8070228930641bd
spences10 <spences10apps@gmail.com>
2025-07-05 17:54:08 +0100

    fix: encode query parameters in object_to_query_params function

--- src/lib/utils/object-to-query-params.ts
+++ src/lib/utils/object-to-query-params.ts
@@ -2,7 +2,8 @@ export const object_to_query_params = (
 	obj: { [s: string]: unknown } | ArrayLike<unknown>,
 ) => {
 	const params = Object.entries(obj).map(
-		([key, value]) => `${key}=${value}`,
+		([key, value]) =>
+			`${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`,
 	)
 	return '?' + params.join('&')
 }
