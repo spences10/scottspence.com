29e62e13af254ef2f80165ebd9b154a0613ec366
spences10 <spences10apps@gmail.com>
2025-10-16 22:08:30 +0100

    fix: update import path for get_subscriber_count to use remote data source

--- src/routes/newsletter/+page.server.ts
+++ src/routes/newsletter/+page.server.ts
@@ -1,4 +1,4 @@
-import { get_subscriber_count } from '$lib/state/subscribers.svelte'
+import { get_subscriber_count } from '$lib/data/subscribers.remote'
 
 export const load = async () => {
 	const { newsletter_subscriber_count } = await get_subscriber_count()
